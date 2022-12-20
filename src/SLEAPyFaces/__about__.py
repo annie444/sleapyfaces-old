5188119bc72029fa9ca4bdc12c7672fc02668847
Annie Ehler <annie.ehler.4@gmail.com>
2022-12-20 10:15:58 -0800

    initial commit

---
+++ src/SLEAPyFaces/__about__.py
@@ -0,0 +1,14 @@
+__version__ = "0.0.1"
+__author__ = "Analetta (Annie) Ehler"
+__author_email__ = "annie.ehler.4@gmail.com"
+__license__ = "MIT"
+__url__ = "https://github.com/annie444/SLEAPyFaces"
+__description__ = "SLEAPyFaces is a Python package for the analysis of SLEAP (Single-LEAP) data of rodent faces."
+__all__ = [
+    "__version__",
+    "__author__",
+    "__author_email__",
+    "__license__",
+    "__url__",
+    "__description__",
+]
