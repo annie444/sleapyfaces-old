18b1aedeff1130a635fb4db411a30cd65d634bda
Annie Ehler <annie.ehler.4@gmail.com>
2022-12-20 10:17:53 -0800

    initial commit

--- src/SLEAPyFaces/__init__.py
+++ src/SLEAPyFaces/__init__.py
@@ -1,3 +1,14 @@
 from __future__ import print_function
 
 from __about__ import *
+
+
+def main():
+    print("SLEAPyFaces version: {}".format(__version__))
+    print("SLEAPyFaces author: {}".format(__author__))
+    print("SLEAPyFaces email: {}".format(__author_email__))
+    print("SLEAPyFaces license: {}".format(__license__))
+
+
+if __name__ == "__main__":
+    main()
