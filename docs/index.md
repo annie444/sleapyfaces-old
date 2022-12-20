ef6cc13a72b1fa4968c3d012cc708cc59d3af6a2
Annie Ehler <annie.ehler.4@gmail.com>
2022-12-20 13:15:54 -0800

    initial commit

---
+++ docs/index.md
@@ -0,0 +1,26 @@
+# SLEAPyFaces
+
+This is a package for extracting facial expressions from SLEAP analyses.
+
+----
+
+[![PyPI - Version](https://img.shields.io/pypi/v/sleapyfaces.svg)](https://pypi.org/project/sleapyfaces)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/sleapyfaces.svg)](https://pypi.org/project/sleapyfaces)
+
+-----
+
+**Table of Contents**
+
+- [SLEAPyFaces](#sleapyfaces)
+	- [Installation](#installation)
+	- [License](#license)
+
+## Installation
+
+```console
+pip install sleapyfaces
+```
+
+## License
+
+`sleapyfaces` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
--- sleapyfaces/cli/__init__.py
+++ sleapyfaces/cli/__init__.py
@@ -6,8 +6,14 @@ import click
 from ..__about__ import __version__
 
 
-@click.group(context_settings={'help_option_names': ['-h', '--help']}, invoke_without_command=True)
-@click.version_option(version=__version__, prog_name='sleapyfaces')
-@click.pass_context
-def sleapyfaces(ctx: click.Context):
-    click.echo('Hello world!')
+@click.group(
+    context_settings={"help_option_names": ["-h", "--help"]},
+    invoke_without_command=True,
+)
+@click.version_option(version=__version__, prog_name="sleapyfaces")
+@click.option("--count", default=1, help="Number of greetings.")
+@click.option("--name", prompt="Your name", help="The person to greet.")
+def hello(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo(f"Hello {name}!")
